14685064f356c5febab50f6bb8e5aafcf66f33f1
ElgaVigrieze <71026101+ElgaVigrieze@users.noreply.github.com>
2022-08-04 13:52:26 +0300

    Lombok annotations added to Ship

--- src/com/company/Ship.java
+++ src/com/company/Ship.java
@@ -1,32 +1,17 @@
 package com.company;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 
-
+@Data
 public class Ship {
     private final int length;
     private final ArrayList<int[]> squares = new ArrayList<>();
 
-
-    public ArrayList<int[]> getSquares() {
-        return squares;
-    }
-
-
-    public Ship(int length) {
-        this.length = length;
-    }
-
-
-    public int getLength() {
-        return length;
-    }
-
     public void setSquares(int[] square) {
         squares.add(square);
     }
-
-
 
 
 }
